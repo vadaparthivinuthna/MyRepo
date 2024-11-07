3d7ac578c9a2f9bde2f15cdbd9f07d0521f67b94
VadaparthiVinuthna <vadaparthivinuthna@gmail.com>
2024-11-07 12:39:06 +0530

    commiting

---
+++ Spring-inffffffffffffffffffffff/Spring-inffffffffffffffffffffff/src/main/java/com/example/Spring_inffffffffffffffffffffff/SpringInffffffffffffffffffffffApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_inffffffffffffffffffffff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInffffffffffffffffffffffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInffffffffffffffffffffffApplication.class, args);
+	}
+
+}
